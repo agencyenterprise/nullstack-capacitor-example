004898696b8ae02c72e35e622c0f9e1efa193cb1
Victor <v.pasetto96@gmail.com>
2022-02-02 10:34:18 -0300

    Added verification function to purchases

---
+++ android/app/src/main/java/com/example/app/plugins/subscriptions/Security.kt
@@ -0,0 +1,63 @@
+package com.example.app.plugins.subscriptions
+
+import android.util.Base64
+import android.util.Log
+
+import com.appsandbox.test.BuildConfig
+import java.io.IOException
+import java.lang.IllegalArgumentException
+import java.security.*
+import java.security.spec.X509EncodedKeySpec
+
+object Security {
+    private const val TAG = "Payment/Security"
+    private const val KEY_FACTORY_ALGORITHM = "RSA"
+    private const val SIGNATURE_ALGORITHM = "SHA1withRSA"
+    private const val PUBLIC_KEY = BuildConfig.SUBSCRIPTION_LICENSE_KEY
+
+    fun verifyPurchase(data: String, signature: String): Boolean {
+        if (signature.isBlank() || data.isBlank() || PUBLIC_KEY.isBlank()) {
+            Log.w(TAG, "Purchase verification failed: missing data.")
+            return false
+        }
+        return try {
+            val key = generatePublicKey(PUBLIC_KEY)
+            verify(key, data, signature)
+        } catch (e: InvalidKeyException) {
+            Log.e(TAG, "")
+            false
+        }
+    }
+
+    private fun generatePublicKey(publicKey: String): PublicKey {
+        val decodedKey = Base64.decode(publicKey, Base64.DEFAULT)
+        val keyFactory = KeyFactory.getInstance(KEY_FACTORY_ALGORITHM)
+        return keyFactory.generatePublic(X509EncodedKeySpec(decodedKey))
+    }
+
+    private fun verify(publicKey: PublicKey, data: String, signature: String): Boolean {
+        val signatureBytes = try {
+            Base64.decode(signature, Base64.DEFAULT)
+        } catch (e: IllegalArgumentException) {
+            Log.w(TAG, "Decoding Failed")
+            return false
+        }
+        try {
+            Signature.getInstance(SIGNATURE_ALGORITHM)
+                .run {
+                    initVerify(publicKey)
+                    update(data.toByteArray())
+                    if (verify(signatureBytes)) {
+                        return true
+                    }
+                }
+            Log.w(TAG, "Signature verification failed")
+            return false
+        } catch (e: InvalidKeyException) {
+            Log.e(TAG, "Invalid Key")
+        } catch (e: SignatureException) {
+            Log.e(TAG, "Signature Exception")
+        }
+        return false
+    }
+}
